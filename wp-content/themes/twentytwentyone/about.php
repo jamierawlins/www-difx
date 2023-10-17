673b303b4b82b1fb5ff311423d15208185a78e16
Jamie <jamie.rawlins1@gmail.com>
2023-10-17 22:25:32 +0100

    Update about.php
    testing acf

--- wp-content/themes/twentytwentyone/about.php
+++ wp-content/themes/twentytwentyone/about.php
@@ -12,16 +12,10 @@
  */
 
 get_header();
+?>
+<?php the_post(); ?>
+<?php the_content(); ?>
+<h2><?php echo esc_html( get_field('heading') ); ?></h2>
 
-/* Start the Loop */
-while ( have_posts() ) :
-	the_post();
-	get_template_part( 'template-parts/content/content-page' );
-
-	// If comments are open or there is at least one comment, load up the comment template.
-	if ( comments_open() || get_comments_number() ) {
-		comments_template();
-	}
-endwhile; // End of the loop.
-
+<?php
 get_footer();
