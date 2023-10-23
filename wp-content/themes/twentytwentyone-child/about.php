d78bf84cddda7aa948d9ca734dea19947a1c5655
Jamie Rawlins <jamie.rawlins@mojofuel.com>
2023-10-23 19:26:43 +0100

    child theme

---
+++ wp-content/themes/twentytwentyone-child/about.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * The template for displaying all single posts
+ *
+ * Template Name: About
+ * 
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
+
+get_header();
+?>
+<?php the_post(); ?>
+<?php the_content(); ?>
+<h2><?php echo esc_html( get_field('simple') ); ?></h2>
+
+<?php
+get_footer();
