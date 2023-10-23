0385fe4cda87269611845b3e06df733bcbdaff8e
Jamie Rawlins <jamie.rawlins@mojofuel.com>
2023-10-23 19:47:28 +0100

    about client repeater

--- wp-content/themes/twentytwentyone/about.php
+++ wp-content/themes/twentytwentyone/about.php
@@ -17,5 +17,28 @@ get_header();
 <?php the_content(); ?>
 <h2><?php echo esc_html( get_field('simple') ); ?></h2>
 
+
+<?php
+
+// Check rows exists.
+if( have_rows('clients-repeater') ):
+
+    // Loop through rows.
+    while( have_rows('clients-repeater') ) : the_row();
+
+        // Load sub field value.
+        $sub_value = get_sub_field('client');
+        // Do something...
+
+    // End loop.
+    endwhile;
+
+// No value.
+else :
+    // Do something...
+endif;
+?>
+
+
 <?php
 get_footer();
