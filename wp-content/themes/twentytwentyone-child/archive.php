fb3e8d8444d76f1a432724290a8f05acd5e26625
Jamie Rawlins <jamie.rawlins@mojofuel.com>
2023-11-21 21:15:10 +0000

    Update archive.php
    loop

--- wp-content/themes/twentytwentyone-child/archive.php
+++ wp-content/themes/twentytwentyone-child/archive.php
@@ -13,72 +13,30 @@ get_header();
 
 $description = get_the_archive_description();
 ?>
-<div class="hero">
-    <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-    <?php twenty_twenty_one_post_thumbnail(); ?>
-</div>
+
 
 
 <?php
 the_archive_description( '<div class="taxonomy-description">', '</div>' );
 ?>
-<?php
- 
- // The Loop
- while ( have_posts() ) : the_post(); ?>
- <article id="post-<?php the_ID(); ?>">	
-					 <div>
-						 
-						 <div class="thumbnail-wrap">
-							 <?php
-							 if ( has_post_thumbnail() ) :
-								 the_post_thumbnail('medium');
-							 endif;
-							 ?>
-						 </div>
-						 <div class="article-container">
-						 
-							 <header class="entry-header">
-								 <a href="<?php the_permalink(); ?>">
-									 <h2><?php the_title(); ?></h2>
-								 </a>
- 
-								 <?php
- 
-									 $the_cat = get_the_category();
-									 $category_name = $the_cat[0]->cat_name;
-									 $category_link = get_category_link( $the_cat[0]->cat_ID );
- 
-									 ?>
-								 
-									 <a class="category-link" href=<?php echo $category_link ?> title=”<?php echo $category_name ?>”  >
-									 <?php echo $category_name ?>
-									 </a>
- 
-								 
-								 
-							 </header>
-							 <div class="entry-content">
-							 
-								 <div class="entry-content">
-								 <?php echo  the_excerpt();?>
-								 </div>
-								  
-							 <a class="button" href="<?php the_permalink(); ?>">
-									 More information
-								 </a>
-							 </div>
-						 </div>
-						 
-					 </div>
-				 </article>   
- <?php endwhile; 
-  
- else: ?>
- <p>Sorry, no posts matched your criteria.</p>
-  
-  
- <?php endif; ?>
+<?php if ( have_posts() ) : ?>
 
-<?php
-get_footer();
+<header class="page-header alignwide">
+	<?php the_archive_title( '<h1 class="page-title">', '</h1>' ); ?>
+	<?php if ( $description ) : ?>
+		<div class="archive-description"><?php echo wp_kses_post( wpautop( $description ) ); ?></div>
+	<?php endif; ?>
+</header><!-- .page-header -->
+
+<?php while ( have_posts() ) : ?>
+	<?php the_post(); ?>
+	<?php get_template_part( 'template-parts/content/content', get_theme_mod( 'display_excerpt_or_full_post', 'excerpt' ) ); ?>
+<?php endwhile; ?>
+
+<?php twenty_twenty_one_the_posts_navigation(); ?>
+
+<?php else : ?>
+<?php get_template_part( 'template-parts/content/content-none' ); ?>
+<?php endif; ?>
+
+<?php get_footer(); ?>
