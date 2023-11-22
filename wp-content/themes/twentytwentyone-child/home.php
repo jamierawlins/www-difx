f26bc3f9d9aa1d202b06b26fc10ba771699ac2fd
Jamie Rawlins <jamie.rawlins@mojofuel.com>
2023-11-22 20:47:29 +0000

    Update home.php
    style

--- wp-content/themes/twentytwentyone-child/home.php
+++ wp-content/themes/twentytwentyone-child/home.php
@@ -52,8 +52,8 @@ get_header();
         ?>
         <div class="element-item transition <?php echo $category->slug;?>" data-category="<?php echo $category->slug;?>">
             <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-                <div class="descrip">
-                    <h2><?php the_title(); ?></h2>
+                <div class="description">
+                    <h2 style="background-color:<?php the_field('colour-one'); ?>"><?php the_title(); ?></h2>
                     <?php twenty_twenty_one_post_thumbnail(); ?>
                  </div>
             </a>
