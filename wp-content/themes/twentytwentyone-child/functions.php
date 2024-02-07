5551f78a5b9567997b9d9050b4d75bfabb6c0db2
Jamie Rawlins <jamie.rawlins@mojofuel.com>
2024-02-07 19:52:58 +0000

    font-family: 'Bebas Neue', sans-serif;

--- wp-content/themes/twentytwentyone-child/functions.php
+++ wp-content/themes/twentytwentyone-child/functions.php
@@ -9,5 +9,5 @@ add_action( 'wp_enqueue_scripts', 'twentytwentyone_styles');
 
 
 function add_google_fonts() {
-	wp_enqueue_style( ' add_google_fonts ', ' https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap', false );}
+	wp_enqueue_style( ' add_google_fonts ', ' https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Open+Sans:wght@300;400;700&display=swap', false );}
 	add_action( 'wp_enqueue_scripts', 'add_google_fonts' );
