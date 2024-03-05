d5152500e247a1efa9d89ebbc849322414271c56
Jamie Rawlins <jamie.rawlins@mojofuel.com>
2024-03-05 20:11:06 +0000

    Update functions.php
    bebas

--- wp-content/themes/twentytwentyone-child/functions.php
+++ wp-content/themes/twentytwentyone-child/functions.php
@@ -10,7 +10,7 @@ add_action( 'wp_enqueue_scripts', 'twentytwentyone_styles');
 
 function wpb_add_google_fonts() {
   
-	wp_enqueue_style( 'wpb-google-fonts', 'https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,700italic,400,700,300', false ); 
+	wp_enqueue_style( 'wpb-google-fonts', 'https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Open+Sans:wght@300;400;700&display=swap', false ); 
 	}
 	  
 	add_action( 'wp_enqueue_scripts', 'wpb_add_google_fonts' );
