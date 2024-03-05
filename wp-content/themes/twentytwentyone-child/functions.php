475a546742aba24af5c6a105d0e6c390954f01fb
Jamie Rawlins <jamie.rawlins@mojofuel.com>
2024-03-05 19:55:29 +0000

    Update functions.php
    google fonts

--- wp-content/themes/twentytwentyone-child/functions.php
+++ wp-content/themes/twentytwentyone-child/functions.php
@@ -9,5 +9,9 @@ add_action( 'wp_enqueue_scripts', 'twentytwentyone_styles');
 
 
 function add_google_fonts() {
-	wp_enqueue_style( ' add_google_fonts ', ' https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Open+Sans:wght@300;400;700&display=swap', false );}
-	add_action( 'wp_enqueue_scripts', 'add_google_fonts' );
+
+
+
+	wp_register_style('add_google_font', 'https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Open+Sans:wght@300;400;700&display=swap', array(), null);
+wp_enqueue_style('add_google_font');
+}
