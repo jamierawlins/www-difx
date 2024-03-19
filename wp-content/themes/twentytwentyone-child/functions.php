a679062d8b9387081c3a88813cd962741b8844da
Jamie Rawlins <jamie.rawlins@mojofuel.com>
2024-03-19 19:46:21 +0000

    Update functions.php

--- wp-content/themes/twentytwentyone-child/functions.php
+++ wp-content/themes/twentytwentyone-child/functions.php
@@ -23,12 +23,15 @@ function wpb_add_google_fonts_bebas() {
 	
 	add_action( 'wp_enqueue_scripts', 'wpb_add_google_fonts_bebas' );
 	
-function add_aos_css() {
-	wp_register_style( 'AOS_style', 'https://cdn.rawgit.com/michalsnik/aos/2.1.1/dist/aos.css' );
-	wp_enqueue_style('AOS_style');
-	add_action( 'wp_enqueue_scripts', 'add_aos_css' );
-
-function add_aos_js() {
-wp_register_script( 'AOS_js', 'https://cdn.rawgit.com/michalsnik/aos/2.1.1/dist/aos.js', null, null, true );
-	wp_enqueue_script('AOS_js');`
-	add_action( 'wp_enqueue_scripts', 'add_aos_js' );
+	function register_scripts_and_styles() {
+		// Enqueue AOS styles
+		wp_enqueue_style(' AOS_animate', 'https://cdn.rawgit.com/michalsnik/aos/2.1.1/dist/aos.css', false, null);
+	
+		// Enqueue AOS script library in footer
+		wp_eqneue_script('AOS', 'https://cdn.rawgit.com/michalsnik/aos/2.1.1/dist/aos.js', false, null, true);
+	
+		// For the JavaScript that initializes AOS, it's best if you manage that in a separate
+		// JS file and enqueue it as a dependency of AOS script library. That way, it ensures the
+		// order of your scripts.
+		wp_('theme-js', 'path/to/theme.js', ['AOS'], null, true);
+	}
