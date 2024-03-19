28b7adc245368fdc92197528948b3e4696f2e817
Jamie Rawlins <jamie.rawlins@mojofuel.com>
2024-03-19 20:21:37 +0000

    Update footer.php

--- wp-content/themes/twentytwentyone-child/footer.php
+++ wp-content/themes/twentytwentyone-child/footer.php
@@ -76,6 +76,7 @@
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
 <script>
     jQuery('.global-media-settings:after').attr({
