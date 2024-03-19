34a99e0cd6009958296444213677ccfc6dfaee4e
Jamie Rawlins <jamie.rawlins@mojofuel.com>
2024-03-19 19:45:36 +0000

    js

--- wp-content/themes/twentytwentyone-child/functions.php
+++ wp-content/themes/twentytwentyone-child/functions.php
@@ -23,9 +23,12 @@ function wpb_add_google_fonts_bebas() {
 	
 	add_action( 'wp_enqueue_scripts', 'wpb_add_google_fonts_bebas' );
 	
-
-wp_register_style( 'AOS_style', 'https://cdn.rawgit.com/michalsnik/aos/2.1.1/dist/aos.css' );
+function add_aos_css() {
+	wp_register_style( 'AOS_style', 'https://cdn.rawgit.com/michalsnik/aos/2.1.1/dist/aos.css' );
 	wp_enqueue_style('AOS_style');
-	
+	add_action( 'wp_enqueue_scripts', 'add_aos_css' );
+
+function add_aos_js() {
 wp_register_script( 'AOS_js', 'https://cdn.rawgit.com/michalsnik/aos/2.1.1/dist/aos.js', null, null, true );
 	wp_enqueue_script('AOS_js');`
+	add_action( 'wp_enqueue_scripts', 'add_aos_js' );
